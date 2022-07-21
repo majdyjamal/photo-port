a03c6c07b102f534df8131ec397ff7c7278ea3e7
Majdy Jamal <majdyjamal@gmail.com>
2022-07-21 22:43:49 +0930

    20.3.6: Test the Gallery-5

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -8,7 +8,7 @@ const Gallery = (props) => {
 
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
             <p>{currentCategory.description}</p>
             <PhotoList category = {currentCategory.name} />
         </section>
